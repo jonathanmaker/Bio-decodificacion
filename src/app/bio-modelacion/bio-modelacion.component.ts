9699f580c0bea011c7f79a9c09a1342d37c8d584
jonathanmaker <jonathantbm2015@gmail.com>
2022-11-30 10:01:07 -0300

    se incorpora hipotesis , bio modelacion y generacion de PDF en ambas

--- src/app/bio-modelacion/bio-modelacion.component.ts
+++ src/app/bio-modelacion/bio-modelacion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-bio-modelacion',
   templateUrl: './bio-modelacion.component.html',
@@ -12,4 +13,17 @@ export class BioModelacionComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  openPDF(): void {
+    let DATA = document.getElementById('htmlData');
+    html2canvas(DATA).then((canvas) => {
+      let fileWidth = 135;
+      let fileHeight = (canvas.height * fileWidth) / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png');
+      let PDF = new jsPDF('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+      console.log('se agrega imagen')
+      PDF.save('ficha-conflictual.pdf');
+    });
+  }
 }
--- src/app/hipotesis/hipotesis.component.ts
+++ src/app/hipotesis/hipotesis.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-hipotesis',
   templateUrl: './hipotesis.component.html',
@@ -12,4 +13,18 @@ export class HipotesisComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  openPDF(): void {
+    let DATA: any = document.getElementById('htmlData');
+    html2canvas(DATA).then((canvas) => {
+      let fileWidth = 208;
+      let fileHeight = (canvas.height * fileWidth) / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png');
+      let PDF = new jsPDF('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+      console.log('se agrega imagen')
+      PDF.save('ficha-conflictual.pdf');
+    });
+  }
 }
